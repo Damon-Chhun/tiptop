278f96c60a66804a0b881f4f7958d95112e06f1d
Damon Chhun <damonchhun7@gmail.com>
2022-04-08 11:50:30 -0700

    feat: styled Navbar with styled-components

--- tiptop/src/components/NavBar/index.js
+++ tiptop/src/components/NavBar/index.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+
+import * as Styled from "./index.styled";
 
 const NavBar = () => {
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Link to="/">
+      <Styled.AppBarStyled position="static">
+        <Styled.ToolBarStyled>
+          <Styled.LinkHomeStyled to="/">
             <Typography variant="h4">TipTop</Typography>
-          </Link>
-          <div>
-            <Link to="shop/*">Shop</Link>
-            <Link to="login/">Login</Link>
-            <Link to="checkout/">Checkout</Link>
-          </div>
-        </Toolbar>
-      </AppBar>
+          </Styled.LinkHomeStyled>
+          <Styled.LinkContainerStyled>
+            <Styled.LinkStyled to="shop/*">Shop</Styled.LinkStyled>
+            <Styled.LinkStyled to="login/">Login</Styled.LinkStyled>
+            <Styled.LinkStyled to="checkout/">Checkout</Styled.LinkStyled>
+          </Styled.LinkContainerStyled>
+        </Styled.ToolBarStyled>
+      </Styled.AppBarStyled>
     </div>
   );
 };
